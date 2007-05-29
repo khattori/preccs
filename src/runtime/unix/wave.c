bbac13638508f324d9126f8ed3bcfef145aff991
khattori <hattori@h7.dion.ne.jp>
2007-05-29 14:03:10 +0000

    Linux版のダミーのwave.cを追加

---
+++ src/runtime/unix/wave.c
@@ -0,0 +1,17 @@
+/**
+ * @file 
+ * @brief サウンド処理(実行時ライブラリ)
+ *
+ * @author Kenta HATTORI
+ * @date   2007/05/29
+ * $Id: wave.c,v 1.1 2006/08/07 09:07:51 hattori Exp $
+ */
+#include "perr.h"
+#include "wave.h"
+
+int prc_WaveOutOpen(int ch, int srate) {
+    perr(PERR_NOTSUP, "prc_WaveOutOpen");
+}
+int prc_WaveInOpen(int ch, int srate) {
+    perr(PERR_NOTSUP, "prc_WaveInOpen");
+}
